837c7cb20797f5e1965dc78c57ba74314499b38d
Avoid <avoid87@bk.ru>
2024-11-24 13:59:53 +0300

    Variables

---
+++ Lesson1/Variables.java
@@ -0,0 +1,23 @@
+package Lesson1;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a; //Декларация переменной (место в памяти)
+        a = 5; //Инизиализация переменной
+
+        int myInt = 5; //Целочисленные
+        short myShort = 32; //До 32 тысяч
+        long myLong = 233; //Длинные значения
+
+        double myDouble = 2.3; //Плавающая точка
+        float myFloat = 2.3f; //Небольшие значения
+
+        char myChar = '@'; //Символы
+
+        boolean myBoolean = true; //Булевые
+
+        byte myByte = 5; //До 8 бит (-128 / +128)
+
+        String myString = "Строка"; //Строки
+    }
+}
